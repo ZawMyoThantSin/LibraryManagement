82e071a7bae194a60d3f40427fbed9677f387de5
ZMTS <zawmyothantzin37@gmail.com>
2023-08-06 16:10:41 +0630

    first commit

---
+++ LibraryManagement/src/main/java/com/jwd39/LibraryManagement/controllers/PageController.java
@@ -0,0 +1,12 @@
+package com.jwd39.LibraryManagement.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+}
---
+++ LibraryManagement/src/test/java/com/jwd39/LibraryManagement/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwd39.LibraryManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
