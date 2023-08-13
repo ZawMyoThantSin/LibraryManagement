5123f7876c564c247b4f5461fb8dbd9dfe5c2056
HtetNadi3 <htetnadi82@gmail.com>
2023-08-13 14:17:31 +0630

    card add

---
+++ LibraryManagement/src/main/java/com/jwd39/LibraryManagement/models/genres.java
@@ -0,0 +1,11 @@
+package com.jwd39.LibraryManagement.models;
+
+import java.util.Date;
+
+public class genres {
+    private int genre_id;
+    private String genre_name;
+
+    public genres() {
+    }
+}
