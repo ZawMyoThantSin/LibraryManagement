f9c704a4b15d2aa49c7014fa26b5195d3ca3c31e
ZawMyoThantSin <zawmyothantzin37@gmail.com>
2023-08-16 21:11:15 +0630

    user view

--- LibraryManagement/src/main/java/com/jwd39/LibraryManagement/controllers/AdminController.java
+++ LibraryManagement/src/main/java/com/jwd39/LibraryManagement/controllers/AdminController.java
@@ -1,14 +1,21 @@
 package com.jwd39.LibraryManagement.controllers;
 
+import com.jwd39.LibraryManagement.daos.AccountDAO;
+import com.jwd39.LibraryManagement.models.Account;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.sql.SQLException;
+import java.util.List;
+
 @Controller
 public class AdminController {
     @GetMapping("/admin/home")
-    public String adminHome(Model model){
-        model.addAttribute("title","Admin Home");
+    public String adminHome(Model model) throws SQLException {
+        List<Account> user = new AccountDAO().allACData();
+        model.addAttribute("users",user );
+
         return"admin/admin.home";
     }
     @GetMapping("/book/create")
--- LibraryManagement/src/main/java/com/jwd39/LibraryManagement/daos/Test.java
+++ LibraryManagement/src/main/java/com/jwd39/LibraryManagement/daos/Test.java
@@ -4,13 +4,12 @@ import com.jwd39.LibraryManagement.helpers.MD5Helper;
 import com.jwd39.LibraryManagement.models.Account;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) throws SQLException {
-        String pass = MD5Helper.encrypt("123hello");
-        System.out.println(pass);
-        AccountDAO dao = new AccountDAO();
-        Account user = dao.acLogIn("test2","123");
-        System.out.println(user);
+        Account user = new Account("Admin","123","zm@gmail.com",1);
+        int status =new AccountDAO().acRegister(user);
+        System.out.println(status==1?"success": "fail");
     }
 }
